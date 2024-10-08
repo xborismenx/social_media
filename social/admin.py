696aea9425f3293426fe7483c1d16626fe5e2395
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-09 00:13:22 +0300

    created models post, likes & added in admin panel

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from social.models import Post, Likes
+
+
+class LikesInline(admin.TabularInline):
+    model = Likes
+    fields = ('user', "liked_at",)
+    readonly_fields = ('liked_at',)
+    extra = 0
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("text", "image",)
+    inlines = [LikesInline]
+
+
+admin.site.register(Likes)
--- social/models.py
+++ social/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Post(models.Model):
+    text = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='posts/')
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
+    liked_at = models.DateTimeField(auto_now_add=True)
+
+
