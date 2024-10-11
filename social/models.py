d20dc24e0325f350561e383c68b8b439625648a3
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-12 01:26:27 +0300

    field image in post model relegated in post image model & in post add field date_posted

--- social/models.py
+++ social/models.py
@@ -4,15 +4,19 @@ from django.db import models
 from user.models import User
 
 
+class PostImage(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to='posts/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
 class Post(models.Model):
     text = models.TextField(max_length=500)
-    image = models.ImageField(upload_to='posts/')
     owner = models.ForeignKey(get_user_model(), models.CASCADE, related_name="owner_post")
+    date_posted = models.DateTimeField(auto_now_add=True)
 
 
 class Likes(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_likes')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_likes')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
     liked_at = models.DateTimeField(auto_now_add=True)
-
-
