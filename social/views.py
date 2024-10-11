2ae8adccb45a0bfbcab45a798ca835f7dd0220fd
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-11 17:47:40 +0300

    created post view set

--- social/views.py
+++ social/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from social.models import Post
+from social.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
