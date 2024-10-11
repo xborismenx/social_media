0bdd2316d029e4a49c5c9c6f35ddcbc4d82e2208
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-12 01:27:49 +0300

    created Image post & likes serializers

--- social/serializers.py
+++ social/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
 
-from social.models import Post
+from social.models import Post, Likes, PostImage
+
+
+class ImagePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ("post", "image", "uploaded_at")
+
+
+class LikesSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Likes
+        fields = ('user', 'liked_at')
 
 
 class PostSerializer(serializers.ModelSerializer):
+    images = ImagePostSerializer(many=True, read_only=True)
     owner = serializers.ReadOnlyField(source="owner.username")
+    likes = LikesSerializer(source="post_likes", many=True)
 
     class Meta:
         model = Post
-        fields = ("id", "text", "image", "owner")
+        fields = ("id", "text", "images", "owner", "likes", "date_posted")
 
     def create(self, validated_data):
         request = self.context.get("request")
