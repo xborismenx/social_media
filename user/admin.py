768ede765ac485b9a97b65fbf70d8745aeff7c3a
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-12 01:28:46 +0300

    added support in admin panel user model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
+from user.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    pass
+
