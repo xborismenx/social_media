48c940a6c1378cc524602837b0279a283023eece
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-13 23:24:44 +0300

    added support media files

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -123,6 +123,10 @@ AUTH_USER_MODEL = "user.User"
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from social_media_api import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/social/', include('social.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
