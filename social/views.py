be8c5bc2e06c83de6be89d0a6851d8467f2ee572
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-14 19:17:13 +0300

    added serializer logic for list and retrieve logic & added action like/unlike post

--- social/views.py
+++ social/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
-from social.models import Post
-from social.serializers import PostSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from social.models import Post, Likes
+from social.serializers import PostListSerializer, PostDetailSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        elif self.action == "retrieve":
+            return PostDetailSerializer
+        return PostListSerializer
+
+    @action(detail=True, methods=["post"])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        if not Likes.objects.filter(user=user, post=post).exists():
+            Likes.objects.create(user=user, post=post)
+            return Response({"status": "post liked"}, status=status.HTTP_201_CREATED)
+        return Response({"status": "post already liked"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=["post"])
+    def unlike(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        like = Likes.objects.filter(user=user, post=post)
+        if like.exists():
+            like.delete()
+            return Response({"status": "post unliked"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"status": "post not liked"}, status=status.HTTP_400_BAD_REQUEST)
