eef2e2e57de716a95f9eb46b89ef26b299143419
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-15 02:04:34 +0300

    added fields in user model

--- user/models.py
+++ user/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    image = models.ImageField(upload_to='users/', null=True)
+    city = models.CharField(max_length=60, blank=True)
+    country = models.CharField(max_length=60, blank=True)
+    birth_date = models.DateField(null=True)
+    bio = models.TextField(max_length=500, blank=True)
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
