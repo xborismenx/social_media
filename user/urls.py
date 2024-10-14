69c9fd7f9d1a8ffa63c2720a00c82815bd02b5d5
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-15 02:09:05 +0300

    added endpoint create/ & me/

--- user/urls.py
+++ user/urls.py
@@ -4,10 +4,14 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 
+from user.views import UserCreateView, UserRetrieveUpdateView
+
 app_name = 'user'
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("create/", UserCreateView.as_view(), name="create_user"),
+    path("me/", UserRetrieveUpdateView.as_view(), name="me"),
 ]
