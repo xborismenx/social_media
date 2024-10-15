0551eb343e943349e0feb5eeb12d53a150a9bd0d
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-16 01:06:37 +0300

    added endpoint all/ with user list view

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 
-from user.views import UserCreateView, UserRetrieveUpdateView
+from user.views import UserCreateView, UserRetrieveUpdateView, UserListView
 
 app_name = 'user'
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("all/", UserListView.as_view(), name="user_list"),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path("create/", UserCreateView.as_view(), name="create_user"),
     path("me/", UserRetrieveUpdateView.as_view(), name="me"),
--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, UserDetailSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -9,9 +11,16 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+class UserListView(generics.ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserDetailSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
