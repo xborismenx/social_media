1776cc7071f1363aad84349f8379da8ef993eb95
boris <boris.tikhonov.21@gmail.com>
2024-10-19 18:33:20 +0300

    created Admin panel
    Signed-off-by: boris <boris.tikhonov.21@gmail.com>

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext as _
-from user.models import User
+from .models import User
 
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    pass
-
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "image", "city", "country", "birth_date", "bio")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "username", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("email",)
