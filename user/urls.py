82d05c7b28801a95e84ab5451461d2d4cb76b5c2
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-23 19:39:49 +0300

    added users/ prefix in endpoints & created routers for UserViewSet
    Signed-off-by: xborismenx <boris.tikhonov.21@gmail.com>

--- user/urls.py
+++ user/urls.py
@@ -1,19 +1,24 @@
+from rest_framework.routers import SimpleRouter
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView, TokenVerifyView, TokenBlacklistView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView,
 )
-from django.urls import path
 
-from user.views import UserCreateView, UserRetrieveUpdateView, UserListView
+from user.views import UserViewSet
+
+router = SimpleRouter()
+router.register(r'users', UserViewSet, basename='users')
 
 app_name = 'user'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path("all/", UserListView.as_view(), name="user_list"),
-    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    path("create/", UserCreateView.as_view(), name="create_user"),
-    path("me/", UserRetrieveUpdateView.as_view(), name="me"),
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('users/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path("", include(router.urls))
 ]
+
