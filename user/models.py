8c311d87a969eef3f2dcc0903a39608286fe7b98
xborismenx <boris.tikhonov.21@gmail.com>
2024-10-23 19:25:23 +0300

    created model Follow
    Signed-off-by: xborismenx <boris.tikhonov.21@gmail.com>

--- user/models.py
+++ user/models.py
@@ -50,3 +50,9 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
