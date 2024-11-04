4866163ec5e97af70427ff281a581f7c4deb22a7
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-04 16:57:54 +0200

    added unique_together in Follow model
    Signed-off-by: xborismenx <boris.tikhonov.21@gmail.com>

--- user/models.py
+++ user/models.py
@@ -56,3 +56,6 @@ class Follow(models.Model):
     follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (("follower", "following"),)
