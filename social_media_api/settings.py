8e6d1ee5d8f0f61f2b2d92e0e2080a46e48c9161
xborismenx <boris.tikhonov.21@gmail.com>
2024-11-05 23:51:06 +0200

    installed python-dotenv & use os.getenv to load SECRET_KEY from .env file
    Signed-off-by: xborismenx <boris.tikhonov.21@gmail.com>

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4p4h9hh31qft_omwwzf=rz3okex1x1dfk%5bac4h+=qn3bjju'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
